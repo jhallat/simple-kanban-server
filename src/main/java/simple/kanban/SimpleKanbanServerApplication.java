bf128bb5c2bc930bb94de27401e2b77791651199
John Hallat <jhallat@gmail.com>
2019-01-18 07:19:30 -0500

    initial code commit

---
+++ src/main/java/simple/kanban/SimpleKanbanServerApplication.java
@@ -0,0 +1,14 @@
+package simple.kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleKanbanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleKanbanServerApplication.class, args);
+	}
+
+}
+
