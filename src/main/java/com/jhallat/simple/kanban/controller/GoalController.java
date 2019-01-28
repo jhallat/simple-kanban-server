a097207304b308e32b893bde7ffe52c53a040d82
John Hallat <jhallat@gmail.com>
2019-01-27 21:59:09 -0500

    added update goal

--- src/main/java/com/jhallat/simple/kanban/controller/GoalController.java
+++ src/main/java/com/jhallat/simple/kanban/controller/GoalController.java
@@ -1,10 +1,13 @@
 package com.jhallat.simple.kanban.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +30,25 @@ public class GoalController {
 	@PostMapping("goals")
 	public Goal addGoal(@RequestBody Goal goal) {
 		return goalRepository.saveAndFlush(goal);
+	}
+	
+	@PutMapping("goals/{id}")
+	public Goal updateGoal(@PathVariable("id") int id, @RequestBody Goal goal) {
+		
+		Optional<Goal> searchResult = goalRepository.findById(id);
+		if (searchResult.isPresent()) {
+			Goal currentGoal = searchResult.get();
+			currentGoal.setAlternatives(goal.getAlternatives());
+			currentGoal.setDescription(goal.getDescription());
+			currentGoal.setExpectation(goal.getExpectation());
+			currentGoal.setStatusId(goal.getStatusId());
+			currentGoal.setStrategy(goal.getStrategy());
+			return goalRepository.saveAndFlush(currentGoal);
+		} else {
+			//TODO This should return an error
+			return addGoal(goal);
+		}
+		
 	}
 
 }
